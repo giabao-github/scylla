53519f78a6cd99abee49f68541cbf698f81f3bf5
giabao-github <giabaonguyen.workspace@gmail.com>
2026-01-19 00:57:47 +0700

    feat: Add initial monorepo setup with web and widget Next.js applications and shared math and ui packages.

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,14 @@
+import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Scylla web</h1>
+        <h1 className="text-2xl font-bold">Hello Scylla widget</h1>
         <Button size="sm">Button</Button>
+        <Input placeholder={`2+2=${add(2, 2)}`} />
       </div>
     </div>
   );
