1af44449470dfa5910fab561ed08977b25b43dbd
giabao-github <giabaonguyen.workspace@gmail.com>
2026-01-19 17:43:14 +0700

    refactor web/app/page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -10,26 +10,32 @@ import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 
-export default function Page() {
+const AuthenticatedContent = () => {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
   return (
+    <div className="flex flex-col gap-y-4 justify-center items-center min-h-svh">
+      <p>Scylla web</p>
+      <div className="flex gap-2">
+        <UserButton />
+        <SignOutButton>
+          <Button variant="outline">Sign out</Button>
+        </SignOutButton>
+      </div>
+      <Button onClick={() => addUser({ name: "Scylla" })}>Add user</Button>
+      <div className="mx-auto w-full max-w-sm">
+        {JSON.stringify(users, null, 2)}
+      </div>
+    </div>
+  );
+};
+
+export default function Page() {
+  return (
     <>
       <Authenticated>
-        <div className="flex flex-col gap-y-4 justify-center items-center min-h-svh">
-          <p>Scylla web</p>
-          <div className="flex gap-2">
-            <UserButton />
-            <SignOutButton>
-              <Button variant="outline">Sign out</Button>
-            </SignOutButton>
-          </div>
-          <Button onClick={() => addUser({ name: "Scylla" })}>Add user</Button>
-          <div className="mx-auto w-full max-w-sm">
-            {JSON.stringify(users, null, 2)}
-          </div>
-        </div>
+        <AuthenticatedContent />
       </Authenticated>
       <Unauthenticated>
         <div className="flex flex-col gap-y-4 justify-center items-center min-h-svh">
