641ac0687056bdde04644509972a8f0f93944dad
giabao-github <giabaonguyen.workspace@gmail.com>
2026-01-19 10:35:32 +0700

    feat: Initialize monorepo with Convex backend and Next.js web and widget applications.

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,19 @@
-import { add } from "@workspace/math/add";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Scylla widget</h1>
-        <Button size="sm">Button</Button>
-        <Input placeholder={`2+2=${add(2, 2)}`} />
+    <div className="flex flex-col gap-y-4 items-center justify-center min-h-svh">
+      <p>Scylla widget</p>
+      <Button onClick={() => addUser({})}>Add user</Button>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );
