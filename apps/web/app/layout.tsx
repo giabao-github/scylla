87904555d838c8d6cbe7ae22d6fdab22d3f68b39
giabao-github <giabaonguyen.workspace@gmail.com>
2026-02-16 23:43:51 +0700

    fix minor issue

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans font-medium antialiased `}
+        className={`${fontSans.variable} ${fontMono.variable} font-sans font-medium antialiased`}
       >
         <ClerkProvider>
           <Providers>{children}</Providers>
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -9,7 +9,7 @@ interface WidgetViewProps {
 
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
   return (
-    <main className="flex overflow-hidden flex-col w-full h-full min-h-screen rounded-xl border min-w-screen bg-muted">
+    <main className="flex overflow-hidden flex-col w-full h-full min-h-screen rounded-xl border bg-muted">
       <WidgetHeader>
         <div className="flex flex-col gap-y-2 justify-between px-2 py-6 font-semibold">
           <p className="text-3xl">Hi there! 👋</p>
